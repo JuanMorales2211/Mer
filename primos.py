22355a4473db6b7d127168d3d8d9988cc135d9e6
JuanMorales2211 <moralesgarzonjuansteeven@gmail.com>
2024-08-22 08:58:57 -0500

    act1

---
+++ primos.py
@@ -0,0 +1,16 @@
+))
+contador = 2
+while ( contador <= numero_primos):
+    numero
+    divisor = 2
+    while ( divisor <= numero):
+        residuo= numero % divisor 
+        if ( residuo == 0) :
+            if (divisor < numero) :
+                divisor = numero +1
+            else:
+                print ( 'es primo', numero )
+                divisor = numero +1
+        divisor += 1
+
+
